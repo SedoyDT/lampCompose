e69e4ecef9daec3bbb865170ab2d7018db4671b0
Anatoliy <mazzzfaka@gmail.com>
2021-11-26 16:38:50 +0300

    чуть-чуть изменений, не задерживайся на php

--- data/public/index.php
+++ data/public/index.php
@@ -4,7 +4,30 @@ $user = 'root';
 $pass = 'root';
 $conn = new mysqli($host, $user, $pass);
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+
+<body>
+<?php
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected to MySQL successfully!";
+?>
+
+<h1><?php echo "Connected to MySQL successfully!"; ?></h1>
+<button id="button" class="goByUrl"">Страница работы с таблицами</button>
+</body>
+<script>
+    let button = document.getElementById("button");
+    button.onclick = function () {
+        window.location.assign("http://localhost:8080/work-with-db/")
+    }
+</script>
+</html>
---
+++ data/public/work-with-db/dbCreator.php
@@ -0,0 +1,12 @@
+<?php
+$login = ($_GET);
+
+class dbCreator
+{
+    public function createDb($login)
+    {
+
+
+        return '1234';
+    }
+}
